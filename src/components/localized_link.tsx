ec61e152192d6944d3972f81cf76e33598ad5e3a
BriceFab <bricefab123@gmail.com>
2025-06-11 21:37:55 +0200

    feat(test): internationalization

--- src/components/localized_link.tsx
+++ src/components/localized_link.tsx
@@ -1,15 +1,26 @@
 'use client';
 
-import Link from 'next/link';
+import Link, { LinkProps } from 'next/link';
 import { useParams } from 'next/navigation';
+import { UrlObject } from 'url';
+
+type LocalizedLinkProps = Omit<React.ComponentProps<typeof Link>, 'href'> & {
+    href: string | UrlObject;
+};
 
 export default function LocalizedLink({
-  href,
-  ...props
-}: React.ComponentProps<typeof Link>) {
+                                          href,
+                                          ...props
+                                      }: LocalizedLinkProps) {
     const { lang } = useParams();
 
-    const localizedHref = `/${lang}${href.startsWith('/') ? href : '/' + href}`;
+    const localizedHref =
+        typeof href === 'string'
+            ? `/${lang}${href.startsWith('/') ? href : '/' + href}`
+            : {
+                ...href,
+                pathname: `/${lang}${(href.pathname ?? '').startsWith('/') ? href.pathname : '/' + href.pathname}`,
+            };
 
     return <Link href={localizedHref} {...props} />;
 }
